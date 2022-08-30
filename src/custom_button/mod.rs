6eccdf4e799d10e2ea24fc3a6b2715ad0477930f
Rongcui Dong <rongcuid@outlook.com>
2022-08-30 09:44:59 -0400

    A custom button

---
+++ src/custom_button/mod.rs
@@ -0,0 +1,19 @@
+mod imp;
+
+use glib::Object;
+use gtk::glib;
+
+glib::wrapper! {
+    pub struct CustomButton(ObjectSubclass<imp::CustomButton>)
+        @extends gtk::Button, gtk::Widget,
+        @implements gtk::Accessible, gtk::Actionable, gtk::Buildable, gtk::ConstraintTarget;
+}
+
+impl CustomButton {
+    pub fn new() -> Self {
+        Object::new(&[]).expect("Failed to create `CustomButton`.")
+    }
+    pub fn with_label(label: &str) -> Self {
+        Object::new(&[("label", &label)]).expect("Failed to create `CustomButton`.")
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use custom_button::CustomButton;
+use gtk::{prelude::*, glib};
+use gtk::{Application, ApplicationWindow, Button};
+use gtk::subclass::prelude::*;
+const APP_ID: &str = "org.gtk_rs.HelloWorld3";
+
+mod custom_button;
+
+fn main() {
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    // Create a button with label and margins
+    let button = CustomButton::with_label("Press me!");
+    button.set_margin_top(12);
+    button.set_margin_bottom(12);
+    button.set_margin_start(12);
+    button.set_margin_end(12);
+
+    // Connect to "clicked" signal of `button`
+    button.connect_clicked(move |button| {
+        // Set the label to "Hello World!" after the button has been clicked on
+        button.set_label("Hello World!");
+    });
+
+    // Create a window
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&button)
+        .build();
+
+    // Present window
+    window.present();
+}
