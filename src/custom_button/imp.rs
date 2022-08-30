8577bf66f168cc086bf6945df2cdd8c79700df53
Rongcui Dong <rongcuid@outlook.com>
2022-08-30 10:21:39 -0400

    Properties

--- src/custom_button/imp.rs
+++ src/custom_button/imp.rs
@@ -1,8 +1,12 @@
 use std::cell::Cell;
 
-use gtk::{glib, subclass::prelude::*, traits::ButtonExt};
-
-
+use gtk::{
+    glib::{self, BindingFlags, ParamSpec, ParamSpecInt, Value},
+    prelude::*,
+    subclass::prelude::*,
+    traits::ButtonExt,
+};
+use once_cell::sync::Lazy;
 
 #[derive(Default)]
 pub struct CustomButton {
@@ -17,15 +21,37 @@ impl ObjectSubclass for CustomButton {
 }
 
 impl ObjectImpl for CustomButton {
+    fn properties() -> &'static [ParamSpec] {
+        static PROPERTIES: Lazy<Vec<ParamSpec>> =
+            Lazy::new(|| vec![ParamSpecInt::builder("number").build()]);
+        PROPERTIES.as_ref()
+    }
+    fn set_property(&self, _obj: &Self::Type, _id: usize, value: &Value, pspec: &ParamSpec) {
+        match pspec.name() {
+            "number" => {
+                let input_number = value.get().expect("The value needs to be of type `i32`.");
+                self.number.replace(input_number);
+            }
+            _ => unimplemented!(),
+        }
+    }
+    fn property(&self, _obj: &Self::Type, _id: usize, pspec: &ParamSpec) -> Value {
+        match pspec.name() {
+            "number" => self.number.get().to_value(),
+            _ => unimplemented!(),
+        }
+    }
     fn constructed(&self, obj: &Self::Type) {
         self.parent_constructed(obj);
-        obj.set_label(&self.number.get().to_string());
+        obj.bind_property("number", obj, "label")
+            .flags(BindingFlags::SYNC_CREATE)
+            .build();
     }
 }
 impl WidgetImpl for CustomButton {}
 impl ButtonImpl for CustomButton {
     fn clicked(&self, button: &Self::Type) {
-        self.number.set(self.number.get() + 1);
-        button.set_label(&self.number.get().to_string())
+        let incremented_number = self.number.get() + 1;
+        button.set_property("number", &incremented_number);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use custom_button::CustomButton;
-use gtk::{prelude::*, glib};
-use gtk::{Application, ApplicationWindow, Button};
+use gtk::glib::BindingFlags;
 use gtk::subclass::prelude::*;
+use gtk::{glib, prelude::*, Align, Orientation};
+use gtk::{Application, ApplicationWindow, Button};
 const APP_ID: &str = "org.gtk_rs.HelloWorld3";
 
 mod custom_button;
@@ -19,17 +20,43 @@ fn main() {
 
 fn build_ui(app: &Application) {
     // Create a button with label and margins
-    let button = CustomButton::new();
-    button.set_margin_top(12);
-    button.set_margin_bottom(12);
-    button.set_margin_start(12);
-    button.set_margin_end(12);
+    let button_1 = CustomButton::new();
+    let button_2 = CustomButton::new();
+
+    button_1
+        .bind_property("number", &button_2, "number")
+        .transform_to(|_, value| {
+            let number = value.get::<i32>().unwrap();
+            let incremented_number = number + 1;
+            Some(incremented_number.to_value())
+        })
+        .transform_from(|_, value| {
+            let number = value.get::<i32>().unwrap();
+            let decremented_number = number - 1;
+            Some(decremented_number.to_value())
+        })
+        .flags(BindingFlags::BIDIRECTIONAL | BindingFlags::SYNC_CREATE)
+        .build();
+
+    // Set up box
+    let gtk_box = gtk::Box::builder()
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .valign(Align::Center)
+        .halign(Align::Center)
+        .spacing(12)
+        .orientation(Orientation::Vertical)
+        .build();
+    gtk_box.append(&button_1);
+    gtk_box.append(&button_2);
 
     // Create a window
     let window = ApplicationWindow::builder()
         .application(app)
         .title("My GTK App")
-        .child(&button)
+        .child(&gtk_box)
         .build();
 
     // Present window
